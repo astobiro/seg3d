74b2d818615615cf68c6f37943e0f9397eea5223
Artur <astobiro@gmail.com>
2022-07-20 15:13:12 -0300

    test

--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -56,6 +56,7 @@ def tversky_loss(delta = 0.7, smooth = 0.000001):
     """
     def loss_function(y_true, y_pred):
         axis = identify_axis(y_true.get_shape())
+        print(axis)
         # Calculate true positives (tp), false negatives (fn) and false positives (fp)   
         tp = K.sum(y_true * y_pred, axis=axis)
         fn = K.sum(y_true * (1-y_pred), axis=axis)
